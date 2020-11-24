e1ce01e99bb5d4989ac498851653f52728fa919d
Yudha13Koley <yudhajit1998@gmail.com>
2020-11-24 11:43:09 +0530

    Added : My first tomcat servlet to display hello world on port 8080

---
+++ src/main/java/com/capgemini/myfirstservlet/SimpleServlet.java
@@ -0,0 +1,22 @@
+package com.capgemini.myfirstservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class SimpleServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h3>Hello World ! Yudhajit Trying Again !</h3>");
+		out.close();
+	}
+}
